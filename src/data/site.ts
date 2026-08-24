b2d297ea0b10e741e4611bd99dbbc541e756c146
Htun Khaing Lynn <htunkhaing.lynn@wavemoney.com.mm>
2026-08-24 16:07:07 +0630

    Add contact mailbox and theme icons

--- src/data/site.ts
+++ src/data/site.ts
@@ -2,6 +2,7 @@ export const siteData = {
   name: 'Htun Khaing Lynn',
   role: 'Platform Developer',
   location: 'Yangon, Myanmar',
+  email: 'htunkhainglynn13@gmail.com',
   description:
     'Platform developer at Wave Money building secure backend systems and APIs for financial services.',
   github: 'https://github.com/htunkhainglynn',
